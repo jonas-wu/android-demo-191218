216894443a22c2e9582bb9ee8274a390b844d7c8
吴玉众 <yuzhong.wu@ucarinc.com>
2019-12-18 09:28:41 +0800

    first commit

---
+++ app/src/main/java/com/jonaswu/androidwithjenkins/MainActivity.java
@@ -0,0 +1,14 @@
+package com.jonaswu.androidwithjenkins;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
